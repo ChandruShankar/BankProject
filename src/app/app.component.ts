282051d55bc553d0dcbeed409aa5ed3abc46943b
Chandru Shankar <88303166+ChandruShankar@users.noreply.github.com>
2021-12-22 11:14:07 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bankproj';
-}
+  constructor(
+    private http:HttpClient
+  ){
+
+  }
+  name:string='';
+  file: any;
+  // getName(name:string){
+  //   this.name=name;
+  // }
+  getfile(event:any){
+    this.file=event.target.files[0];
+    console.log('file',this.file);
+  }
+  submitData(){
+    let formData = new FormData();
+    // formData.set('name',this.name);
+    formData.set('file',this.file);
+    //submit this data in API
+    this.http.post('',formData)
+    .subscribe((response)=>{});
+  }
+  
+};
+
